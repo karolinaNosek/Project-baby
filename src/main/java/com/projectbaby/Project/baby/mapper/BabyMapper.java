246cd95b103ef1036f9d8f61dfc0dce03c0efa19
karolinaNosek <karolina_0607@o2.pl>
2023-04-23 20:10:51 +0200

    user service & controller changes saved, mapping #ignore added, some annotations have been removed

--- src/main/java/com/projectbaby/Project/baby/mapper/BabyMapper.java
+++ src/main/java/com/projectbaby/Project/baby/mapper/BabyMapper.java
@@ -8,8 +8,10 @@ import org.mapstruct.Mapping;
 @Mapper (componentModel = "spring")
 public interface BabyMapper {
     @Mapping(target = "activities", ignore = true)
+    @Mapping(target = "user", ignore = true)
     BabyDTO mapToBabyDTO (Baby baby);
     @Mapping(target = "activities", ignore = true)
+    @Mapping(target = "user", ignore = true)
     Baby mapToBaby (BabyDTO babyDTO);
 
 
--- src/main/java/com/projectbaby/Project/baby/mapper/UserMapper.java
+++ src/main/java/com/projectbaby/Project/baby/mapper/UserMapper.java
@@ -3,9 +3,12 @@ package com.projectbaby.Project.baby.mapper;
 import com.projectbaby.Project.baby.model.dto.UserDTO;
 import com.projectbaby.Project.baby.model.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+    @Mapping(target = "baby", ignore = true)
     UserDTO mapToUserDTO (User user);
+    @Mapping(target = "baby", ignore = true)
     User mapToUser (UserDTO userDTO);
 }
